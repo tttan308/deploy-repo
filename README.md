c4c47aaa8e63c8c899a934c4e670c3842907f5bb
tantran <tantran.300803@gmail.com>
2024-11-29 01:31:47 +0700

    Remove unused SSL configuration and deployment scripts

--- README.md
+++ README.md
@@ -6,12 +6,10 @@
 # 1. Clone the repository.
 git clone 
 
-# 2. Enter your alia-professional-be folder.
+# 2. Enter your 
 cd 
 
 # 3. Change Webserver & SSL config
-- webserver/nginx/app.conf: Replace <domain> -> domain deployment
-- webserver/init-letsencrypt.sh: Replace <domain> -> domain deployment
 
 # 4. Run first time.
 make bootstrap
--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e
 
-backend_service=backend   # Tên service backend
-frontend_service=frontend # Tên service frontend
-migration_command="npm run migrate:run:prod" # Lệnh chạy migration
+backend_service=backend   
+frontend_service=frontend
+migration_command="npm run migrate:run:prod"
 
 deploy() {
   if [ -z "$1" ]; then
-    # Yêu cầu nhập version
     read -r -p "Enter version you want to deploy: " version
   else
     version=$1
   fi
 
-  # Kiểm tra version có hợp lệ không
   if [ -z "$version" ]; then
     echo "No version provided."
     exit 1
@@ -22,15 +20,12 @@ deploy() {
   export VERSION=$version
   echo "Deploying version $version..."
 
-  # Chạy migration trước cho backend
   echo "Running backend migrations..."
   docker compose run --rm $backend_service $migration_command
 
-  # Deploy backend
   echo "Deploying backend..."
   docker compose up -d --no-deps --scale $backend_service=1 --no-recreate $backend_service
 
-  # Kiểm tra container backend đang chạy
   echo "Checking backend container..."
   backend_container_id=$(docker ps -f name=$backend_service -q | head -n1)
   if [ -z "$backend_container_id" ]; then
@@ -38,7 +33,6 @@ deploy() {
     exit 1
   fi
 
-  # Kiểm tra cổng dịch vụ backend (3030)
   echo "Checking backend on port 3030..."
   backend_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $backend_container_id)
   echo $backend_container_ip
@@ -48,11 +42,9 @@ deploy() {
   fi
   echo "Backend deployed successfully."
 
-  # Deploy frontend
   echo "Deploying frontend..."
   docker compose up -d --no-deps --scale $frontend_service=1 --no-recreate $frontend_service
 
-  # Kiểm tra container frontend đang chạy
   echo "Checking frontend container..."
   frontend_container_id=$(docker ps -f name=$frontend_service -q | head -n1)
   if [ -z "$frontend_container_id" ]; then
@@ -60,7 +52,6 @@ deploy() {
     exit 1
   fi
 
-  # Kiểm tra cổng dịch vụ frontend (3000)
   echo "Checking frontend on port 3000..."
   frontend_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $frontend_container_id)
   if ! nc -z $frontend_container_ip 3000; then
